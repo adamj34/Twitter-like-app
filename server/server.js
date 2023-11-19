2ae7e66079df04e030500189d26b919862303a6b
addar <trashci18@gmail.com>
2023-11-19 19:04:24 +0100

    added request body parsing

--- server/server.js
+++ server/server.js
@@ -20,6 +20,7 @@ const options = {
 const app = express();
 app.use(cors());
 app.use(express.json( {limit: '10mb'} )); 
+app.use(express.urlencoded({ extended: true }));
 
 
 app.use('/signIn', signInRouter);
